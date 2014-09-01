1901e1e6d65d923a8157b973f527d1515af7d519
darkoverlordofdata <darkoverlordofdata@gmail.com>
2014-09-01 00:46:13 -0700

    publish

---
+++ main.dart
@@ -0,0 +1,30 @@
+library alienzed;
+import "package:alienzed/phaser.dart";
+
+import "dart:html";
+import 'dart:math';
+
+
+part "match3/VoidObject.dart";
+part "match3/Piece.dart";
+part "match3/Grid.dart";
+//
+part "alienzed/Alienzed.dart";
+part "alienzed/Assets.dart";
+part "alienzed/GameOver.dart";
+part "alienzed/Gem.dart";
+part "alienzed/GemGroup.dart";
+part "alienzed/Initialize.dart";
+part "alienzed/Level1.dart";
+part "alienzed/Levels.dart";
+
+
+
+main() {
+  print("start");
+
+
+  Game game = new Alienzed();
+
+
+}

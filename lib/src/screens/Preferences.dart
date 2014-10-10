f1a749c64648558890649abc99d27a2d571e64fd
darkoverlordofdata <darkoverlordofdata@gmail.com>
2014-10-09 20:57:39 -0700

    daily

--- lib/src/screens/Preferences.dart
+++ lib/src/screens/Preferences.dart
@@ -19,20 +19,92 @@ class Preferences extends Li2State {
 
   Li2Config config;
   Li2Template template;
+  JsObject _prefs = null;
+  var preferences = {};
 
   /**
    * Preferences use HTML ui
    */
   Preferences(this.config, this.template) {
     print('Preferences Class initialized');
+
+    /**
+     * make sure that booleans are type bool
+     */
+    xform(value) {
+      switch(value) {
+        case 'true':  return true;
+        case 'false': return false;
+        default:      return value;
+      }
+    }
+    if (context['plugins'] != null)_prefs = context['plugins']['appPreferences'];
+
+    // get the saved preferences
+    for (var category in config.preferences['categories']) {
+      category['preferences'].forEach((preference) {
+        String key = preference['key'];
+        preferences[key] = preference;
+
+        if (_prefs == null) {
+          preference['value'] = window.localStorage[key] != null
+          ? xform(window.localStorage[key])
+          : preference['defaultValue'];
+        } else {
+          _prefs.callMethod('fetch', [
+                  (value) => (preference['value'] = xform(value)),
+                  (error) => (preference['value'] = preference['defaultValue']),
+              key]);
+        }
+      });
+    }
   }
 
+  /**
+   * Start
+   */
   create() {
 
     querySelector(config.preferences['id'])
       ..setInnerHtml(template.render(config.preferences))
       ..style.display = 'inline';
 
+    querySelector('.holo-action-bar').on['click'].listen((event){
+      querySelector(config.preferences['id'])
+        ..style.display = 'none';
+      state.start(config.menu);
+    });
+
+    querySelector('.holo-action-bar').on['touchstart'].listen((event){
+      querySelector(config.preferences['id'])
+        ..style.display = 'none';
+      state.start(config.menu);
+    });
+
+    preferences.forEach((key, preference) {
+      querySelector("#$key").on['change'].listen((event) {
+        var value;
+        String key = event.target.id;
+        switch(event.target.type) {
+          case 'checkbox':
+            value = event.target.checked;
+            break;
+          case 'select-one':
+            value = event.target.value;
+            break;
+          default:
+            value = "";
+        }
+        if (_prefs == null) {
+          window.localStorage[event.target.id] = "$value";
+        } else {
+          _prefs.callMethod('store', [
+                  (value) => (preference['value'] = value),
+                  (error) => (print("Unable to save $key")),
+              key, value]);
+        }
+      });
+    });
   }
 }
 

69cd662dafeef3974838a0ea91994747d5cfb978
darkoverlordofdata <darkoverlordofdata@gmail.com>
2014-10-18 13:51:41 -0700

    publish

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('window.html', {
-        'id': 'd16a',
-        'outerBounds': {
-            'width': 512,
-            'height': 768
-        }
-    });
-});

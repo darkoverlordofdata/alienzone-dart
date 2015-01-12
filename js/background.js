6f6d8b19425e4778827fa69838b8b06c979bd4e7
darkoverlordofdata <darkoverlordofdata@gmail.com>
2015-01-11 22:27:32 -0800

    publish

--- js/background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('index.html', {
-        'id': 'd16a',
-        'outerBounds': {
-            'width': 400,
-            'height': 600
-        }
-    });
-});

3d060abeed04b85a16689f8287781b719bd32d9a
darkoverlordofdata <darkoverlordofdata@gmail.com>
2015-01-21 12:01:25 -0800

    daily

--- zip.sh
+++ zip.sh
@@ -3,19 +3,52 @@
 #   Pack into zip and copy to sdcard for CocoonJS Player
 #
 rm -f alienzone.zip
-cd build/web
 
-vulcanize -s  --inline --csp --output bootstrap.html index.html
+#
+#   switch to the game app
+#
+cd build/web/app
+#
+#   directly load the javascript
+#
+sed -i 's/<script type="text\/javascript"  src="packages\/browser\/dart.js"><\/script>//' index.html
+sed -i 's/<script type="application\/dart" src="main.dart"><\/script>/<script src="main.dart.js"><\/script>/' index.html
+#
+#   compress it some more
+#
+vulcanize -s --inline --csp --output alienzone.html index.html
+rm -f index.html
+mv -f alienzone.html index.html
+rm -f main.dart.js
+rm -fr js
+
+#
+#   switch to the bootstraper
+#
+cd ..
+#
+#   compress it some more
+#
+vulcanize -s  -inline --csp --output bootstrap.html index.html
 rm -f index.html
 mv -f bootstrap.html index.html
 rm -fr js
 
-rm -fr app/packages
-mv -f packages app/packages
 #
-# zip it up
+#   clean up the dart dependencies
+#
+rm -fr app/packages
+rm -fr packages/browser
+mv -f packages app/packages
+
+#
+#   zip it up
 #
 zip -r ../../alienzone.zip .
 cd ..
 cd ..
+
+#
+#   and copy to the device for testing
+#
 adb push alienzone.zip /sdcard/alienzone.zip
